ac9c4a6d4813bc7fbd48ba262447f5546d1e3a99
brownandblue49 <64905307+brownandblue49@users.noreply.github.com>
2020-05-12 04:35:46 +0530

    Update settings.py

--- wsinterfaceproject/settings.py
+++ wsinterfaceproject/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print "base dir path", BASE_DIR
+ABC = os.path.dirname(os.path.dirname(__file__))
+print "ABC-", ABC
 #STAT=os.path.join(BASE_DIR, 'static')
 
 
