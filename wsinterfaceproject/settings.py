81e6a50b963005cf154caf0d35e54f6c7ea73680
brownandblue49 <64905307+brownandblue49@users.noreply.github.com>
2020-05-12 03:21:07 +0530

    Update settings.py

--- wsinterfaceproject/settings.py
+++ wsinterfaceproject/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
  
 STATICFILES_DIRS = (
     
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'files'),
 )
  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
